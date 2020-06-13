0af2a87795c3934659c59c7cba597dc4cc8b4fd9
vcsheel <viveksheel96@gmail.com>
2020-06-13 21:42:44 +0530

    initial commit

---
+++ src/test/java/com/learn/springmvcrest/SpringMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springmvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
