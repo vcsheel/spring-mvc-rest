594ea2162718391b960d6ca25db825997a223230
vcsheel <viveksheel96@gmail.com>
2020-06-14 11:11:51 +0530

    Added CategoryController

---
+++ src/test/java/com/learn/springmvcrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,69 @@
+package com.learn.springmvcrest.controllers;
+
+import com.learn.springmvcrest.api.v1.model.CategoryDTO;
+import com.learn.springmvcrest.services.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryControllerTest {
+
+    @Mock
+    CategoryService categoryService;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @Test
+    void getAllCategories() throws Exception {
+        List<CategoryDTO> categoryDTOS = Arrays.asList(new CategoryDTO(), new CategoryDTO());
+
+        when(categoryService.getAllCategories()).thenReturn(categoryDTOS);
+
+        mockMvc.perform(get("/api/v1/categories/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.categories", hasSize(2)));
+    }
+
+    @Test
+    void getCategoryByName() throws Exception {
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setName("Rob");
+        categoryDTO.setId(1L);
+
+        when(categoryService.getCategoryByName(anyString())).thenReturn(categoryDTO);
+
+        mockMvc.perform(get("/api/v1/categories/Rob").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("Rob")));
+
+    }
+}
